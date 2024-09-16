133174271794ab2720339d75746fe6a7083a1a4c
Travelle Whitney <travellewhitney@gmail.com>
2024-09-15 20:18:47 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+console.log(mod);
+
+const numbers = [1, 5, 10, 20, 3, 7];
+const max = Math.max(...numbers);
+
+console.log(max);
